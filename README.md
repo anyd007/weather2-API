2a7cc537de553be195963ad9010dccab0e476be3
Andrzej Gebala <andrzej.gebala35@gmail.com>
2021-10-26 14:20:58 +0200

    usprawnienia

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # weather2-API
 Aplikacja pogodowa która odzwierciedla obecnie panujące warunki pogodowe, po wpisaniu złej nazwy miasta pojawia się komunikat informujący o tym. 
 Wykorzystane API: https://openweathermap.org/
+Do pobrania API użyłem AXIOS, który zwrócił gotowy obiekt, bez konieczności zmienia go na json
--- script.js
+++ script.js
@@ -6,27 +6,57 @@ const photo = document.querySelector(".photo");
 const weather = document.querySelector(".weather");
 const temperature = document.querySelector(".temperature");
 const humidity = document.querySelector(".humidity");
-const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
-const API_KEY = '&appid=d2b6cbb301cc9b82439cc488b350ee22'
-const API_UNITS = '&units=metric'
-const API_LANG = '&lang=pl'
+const API_LINK = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "&appid=d2b6cbb301cc9b82439cc488b350ee22";
+const API_UNITS = "&units=metric";
+const API_LANG = "&lang=pl";
 
-const getCities = () =>{
-    const cities = input.value || 'Warszawa'
-    const URL = API_LINK + cities + API_KEY + API_UNITS + API_LANG
-    axios.get(URL)
-    .then(res =>{
-        console.log(res.data);
-        const temp = res.data.main.temp
-        const hum = res.data.main.humidity
-        const current = Object.assign({}, ...res.data.weather)
-        
+const getCities = () => {
+  const cities = input.value || "Warszawa";
+  const URL = API_LINK + cities + API_KEY + API_UNITS + API_LANG;
+  axios.get(URL).then((res) => {
+    console.log(res.data);
+    const temp = res.data.main.temp;
+    const hum = res.data.main.humidity;
+    const status = Object.assign({}, ...res.data.weather);
 
-        weather.textContent = current.main
-        temperature.textContent = Math.floor(temp) + '°C'
-        humidity.textContent = hum + '%'
-    })
-}
-getCities()
+    cityName.textContent =
+      input.value.charAt(0).toUpperCase() + input.value.slice(1);
+    weather.textContent = status.main;
+    temperature.textContent = Math.floor(temp) + "°C";
+    humidity.textContent = hum + "%";
 
-button.addEventListener('click', getCities)
+    if (status.id >= 200 && status.id <= 232) {
+      photo.setAttribute("src", "./img/thunderstorm.png");
+      weather.textContent = "Burzowo";
+    } else if (status.id >= 300 && status.id <= 321) {
+      photo.setAttribute("src", "./img/drizzle.png");
+      weather.textContent = "Mżawka";
+    } else if (status.id >= 500 && status.id <= 531) {
+      photo.setAttribute("src", "./img/rain.png");
+      weather.textContent = "Deszczowo";
+    } else if (status.id >= 600 && status.id <= 622) {
+      photo.setAttribute("src", "./img/snow.png");
+      weather.textContent = "Śnieg";
+    } else if (status.id >= 701 && status.id <= 781) {
+      photo.setAttribute("src", "./img/fog.png");
+      weather.textContent = "Mgła";
+    } else if (status.id === 800) {
+      photo.setAttribute("src", "./img/sun.png");
+      weather.textContent = "Słonecznie";
+    } else if (status.id >= 801 && status.id <= 804) {
+      photo.setAttribute("src", "./img/cloud.png");
+      weather.textContent = "Pochmurno";
+    } else {
+      photo.setAttribute("src", "./img/unknown.png");
+    }
+  });
+};
+
+const enterEvent = (e) => {
+  if (e.key === "Enter") {
+    getCities();
+  }
+};
+input.addEventListener("keyup", enterEvent);
+button.addEventListener("click", getCities);
